5cd0754bcc3552f5b79fe3f8530635ae681fe79d
AshishSharma2953 <ashishsharma.2953@gmail.com>
2025-03-09 14:11:16 +0530

    Initail files

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("togglePassword").addEventListener("click", function () {
+    let passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        this.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        this.classList.replace("fa-eye-slash", "fa-eye");
+    }
+});
