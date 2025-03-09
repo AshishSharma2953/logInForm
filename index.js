2f0d3eb8bdf3642a8af8182f206fa3ce08110f04
AshishSharma2953 <ashishsharma.2953@gmail.com>
2025-03-09 14:35:05 +0530

    change icon style

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ document.getElementById("togglePassword").addEventListener("click", function () 
     let passwordField = document.getElementById("password");
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        this.classList.replace("fa-eye", "fa-eye-slash");
+        this.classList.replace("fa-eye-slash", "fa-eye");
+
     } else {
         passwordField.type = "password";
-        this.classList.replace("fa-eye-slash", "fa-eye");
+        this.classList.replace("fa-eye", "fa-eye-slash");
+
     }
 });
